4edb5531aebde5146a070e5a183a7a8990211bf9
Quingu <gustavoquintiliano06@gmail.com>
2026-09-22 18:59:02 -0300

    fix: exporta ConsentimentoTermos no __init__ dos modelos

--- backend/users/models/__init__.py
+++ backend/users/models/__init__.py
@@ -1,2 +1,5 @@
 from .usuario import Usuario
 from .token_recuperacao import TokenDeRecuperacao
+from .consentimento_termos import ConsentimentoTermos
+from .log_acesso import LogAcesso
+from .solicitacao_direito_titular import SolicitacaoDireitoTitular
